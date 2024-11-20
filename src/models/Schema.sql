e6536356560ad40770546466cd88e4433a85340e
Thiago Tanaka Peczek <127541175+Thiago-Peczek@users.noreply.github.com>
2024-11-20 15:50:50 -0300

    Add files via upload

---
+++ src/models/Schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS cardapio;
+CREATE DATABASE cardapio;
+USE cardapio;
+
+CREATE TABLE IF NOT EXISTS tipos(
+	idTipo INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL
+);
+
+INSERT INTO tipos(nome) VALUES 
+('Prato principal'),
+('Sobremesa'),
+('Entrada'),
+('Bebida');
+
+CREATE TABLE IF NOT EXISTS pratos(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	descricao VARCHAR(255) NOT NULL,
+	preco DECIMAL(10,2) NOT NULL,
+	tipo INT NOT NULL,
+	FOREIGN KEY (tipo) REFERENCES tipos(idTipo)
+);
+
+SHOW TABLES;
+DESCRIBE tipos;
+DESCRIBE pratos;
